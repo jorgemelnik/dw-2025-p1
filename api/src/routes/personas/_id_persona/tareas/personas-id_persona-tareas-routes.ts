d862df8567839694d461c6f1051c1ead5637a0ad
Paula Tomas <paulatomas1901@gmail.com>
2025-10-03 20:42:50 -0300

    bueno se hizo lo que se pudo no mucho jeje me salieron millones de errores, nose nos vemos el proximo curso calculo

--- api/src/routes/personas/_id_persona/tareas/personas-id_persona-tareas-routes.ts
+++ api/src/routes/personas/_id_persona/tareas/personas-id_persona-tareas-routes.ts
@@ -1,7 +1,52 @@
-import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import  { Type, type FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import { Persona } from "../../../../model/persona.ts";
+import { createPerson, findAll } from "../../../../services/personas.ts";
+import { ErrorSchema } from "../../../../model/errors.ts";
+
 
 const personasRoutes: FastifyPluginAsyncTypebox = async function (fastify) {
-  //TODO:
-};
+  fastify.get(
+      "/",
+      {
+        schema: {
+          tags: ["personas"],
+          params: Type.Pick(Persona, ["id_persona"], ) ,
+          response: {
+            200: Type.Array(Persona),
+          },
+        },
+        onRequest: [fastify.checkIsAdmin],
+      },
+      async function () {
+        return findAll();
+      }
+    );
+  
+    fastify.post(
+      "/",
+      {
+        schema: {
+          tags: ["personas"],
+          body: Type.Pick(Persona, ["username", "roles"], {
+            examples: [
+              {
+                username: "user5",
+                roles: ["normal"],
+              },
+            ],
+          }),
+          response: {
+            201: Persona,
+            400: ErrorSchema,
+          },
+        },
+        onRequest: [fastify.checkIsAdmin],
+      },
+      async function (req, rep) {
+        rep.code(201);
+        return createPerson(req.body.username, req.body.roles);
+      }
+    );
+  };
 
 export default personasRoutes;
--- front/front.ts
+++ front/front.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import { dirname, join } from "path";
+import fastifyAutoload from "@fastify/autoload";
+import fastifyStatic from "@fastify/static"
 
 const fastify = Fastify({
   logger: true,
@@ -8,7 +10,16 @@ const fastify = Fastify({
 const port: number = parseInt(process.env.FASTIFY_PORT || "3000");
 const host: string = "::";
 
-const rootDir = dirname(process.argv[1]);
+const rootDir = join(dirname(process.argv[1]), "public");
+
+await fastify.register(fastifyStatic,{
+  root: rootDir,
+  prefix:"/"
+})
+
+await fastify.register(fastifyAutoload,{
+  dir: join(rootDir,"scripts", "services")
+})
 
 try {
   await fastify.listen({ host, port });
